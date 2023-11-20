6d962ea6c8615703bc10ce8784718c833fa303a3
K-macc <mkeshawn107@gmail.com>
2023-11-20 18:33:30 -0500

    hey

--- world.js
+++ world.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (create) {
-    var lookcountry = document.getElementById('lookupcountry');
-    const lookcity = document.getElementById('lookcities');
+    const lookcountry = document.getElementById('lookupcountry');
+    const lookcity = document.getElementById('lookupcities');
     var result = document.getElementById('result');
     var country = document.getElementById('country');
 
@@ -9,31 +9,16 @@ document.addEventListener('DOMContentLoaded', function (create) {
         fetch('world.php?country=' + country.value)
             .then(response => response.text())
             .then(countrylist => {
-                //displayData(countrylist);
                 result.innerHTML = countrylist;
             })
+    })
 
-        /*function displayData(data) {
-            result.textContent = "";
-            var str = "";
-            var lst = document.createElement('ul');
-            for (var i = 11; i < data.length; i++) {
-                if (data[i] == "<") {
-                    i += 11;
-                    lst.appendChild(createElement(str));
-                    result.appendChild(lst);
-                    str = "";
-                    continue;
-                } else {
-                    str += data[i];
-                }
-            }
-        }
-
-        function createElement(data) {
-                var item = document.createElement('li');
-                item.appendChild(document.createTextNode(data));
-                return item;
-        }*/
+    lookcity.addEventListener("click", function (e) {
+        e.preventDefault();
+        fetch('world.php?country=' + country.value + '&lookup=cities')
+            .then(response => response.text())
+            .then(countrylist => {
+                result.innerHTML = countrylist;
+            })
     })
 })

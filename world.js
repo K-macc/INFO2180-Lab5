602c2797da834a3ed311707529b4eaa7b8986397
K-macc <mkeshawn107@gmail.com>
2023-11-20 17:03:20 -0500

    Updated lookup search results to table

---
+++ world.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function (create) {
+    const lookcountry = document.getElementById('lookupcountry');
+    const lookcity = document.getElementById('lookcities');
+    var result = document.getElementById('result');
+    var country = document.getElementById('country');
+
+    lookcountry.addEventListener("click", function (e) {
+        e.preventDefault();
+        fetch('world.php?country=' + country.value)
+            .then(response => response.text())
+            .then(countrylist => {
+                //displayData(countrylist);
+                result.innerHTML = countrylist;
+            })
+
+        /*function displayData(data) {
+            result.textContent = "";
+            var str = "";
+            var lst = document.createElement('ul');
+            for (var i = 11; i < data.length; i++) {
+                if (data[i] == "<") {
+                    i += 11;
+                    lst.appendChild(createElement(str));
+                    result.appendChild(lst);
+                    str = "";
+                    continue;
+                } else {
+                    str += data[i];
+                }
+            }
+        }
+
+        function createElement(data) {
+                var item = document.createElement('li');
+                item.appendChild(document.createTextNode(data));
+                return item;
+        }*/
+    })
+})
